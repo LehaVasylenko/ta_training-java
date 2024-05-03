7dd56ab5f00c98273e9eb5c02e8e22ed1867d36a
Oveksii Vasylenko <cradleech@gmail.com>
2024-05-03 20:19:41 +0300

    lets start the game

---
+++ src/main/java/org/epam/learning/Main.java
@@ -0,0 +1,12 @@
+package org.epam.learning;
+
+/**
+ * Default (Template) Project
+ * Author: Vasylenko Oleksii
+ * Date: 03.05.2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
